318ce5c47514d42ce5b1a97e560458240167c8e4
xseignard <xavier.seignard@gmail.com>
2018-02-21 18:07:01 +0100

    🍦 extracting data

---
+++ src/bookeo.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const baseUrl = 'https://api.bookeo.com/v2/bookings/';
+const keysParams = `?apiKey=${process.env.BOOKEO_API_KEY}&secretKey=${
+	process.env.BOOKEO_SECRET_KEY
+}`;
+
+const getBookeoDetails = (bookingId, cb) => {
+	const url = `${baseUrl}${bookingId}${keysParams}`;
+	request({ url, json: true }, (err, res, body) => {
+		if (!err && res.statusCode === 200) {
+			const data = {
+				persons: body.participants.numbers[0].number,
+				room: body.productName,
+				startTime: body.startTime,
+				endTime: body.endTime,
+				creationTime: body.creationTime,
+				title: body.title,
+			};
+			cb(null, data);
+		} else cb({ err: 'Failed to get bookeo details' });
+	});
+};
+
+module.exports = getBookeoDetails;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const getBookeoDetails = require('./bookeo');
+
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
 
@@ -22,11 +24,46 @@ app.post('/', (req, res) => {
 		body.type === 'charge.succeeded' &&
 		body.data.object.application === process.env.BOOKEO_APP
 	) {
-		console.log(body.data.object);
-		console.log(body.data.object.source);
-		console.log('===============');
+		const customerId = body.data.object.customer;
+		stripe.customers.retrieve(customerId, (err, customer) => {
+			if (err) {
+				console.log(err);
+				res.sendStatus(500);
+			} else {
+				const charge = body.data.object;
+				const source = body.data.object.source;
+				const bookingId = customer.description.replace(
+					'Leave in Time Nantes - Réservation ',
+					''
+				);
+				getBookeoDetails(bookingId, (err, data) => {
+					if (err) {
+						console.log(err);
+						res.sendStatus(500);
+					} else {
+						console.log(`Customer decription: ${customer.description}`);
+						console.log(`Customer email: ${customer.email}`);
+						console.log(`Bookeo booking id: ${bookingId}`);
+						console.log(`Stripe charge id: ${charge.id}`);
+						console.log(`Receipt email: ${charge.receipt_email}`);
+						console.log(`Charge description: ${charge.description}`);
+						console.log(`Amount: ${charge.amount / 100} ${charge.currency}`);
+						console.log(`Source name: ${source.name}`);
+						console.log(
+							`Source address:
+						${source.address_line1}
+						${source.address_line2}
+						${source.address_zip} ${source.address_city}
+						${source.address_country}`
+						);
+						console.log(data);
+						console.log('======================');
+						res.sendStatus(200);
+					}
+				});
+			}
+		});
 	}
-	res.sendStatus(200);
 });
 
 app.listen(3300, () => console.log('Running on port 3300 ...'));
